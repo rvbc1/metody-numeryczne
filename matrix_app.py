3f985ce9a1a18bd0c60f23e9c582e56c73909b8f
Marek Knosala <marek_knosala@vp.pl>
2024-03-17 18:48:07 +0100

    some cleanup

--- matrix_app.py
+++ matrix_app.py
@@ -4,9 +4,9 @@ from matrix_data_manager import MatrixDataManager
 from matrix_calculations import MatrixCalculations
 
 
-entry_bg = 'white'  # białe tło dla widoczności
-button_bg = 'grey'  # szare tło dla przycisku
-text_color = 'black'  # czarny tekst dla widoczności
+entry_bg = 'white' 
+button_bg = 'grey'  
+text_color = 'black'  
 
 
 class GuiMatrix():
@@ -19,7 +19,7 @@ class GuiMatrix():
 
 
 
-    def update_size(self, rows, columns):
+    def setSize(self, rows, columns):
         for widget in self.frame.winfo_children():
             widget.destroy()
 
@@ -33,6 +33,47 @@ class GuiMatrix():
             entries.append(row)
         self.entries = entries
 
+    def getRows(self):
+        return self.rows
+    
+    def getColumns(self):
+        return self.columns
+
+    def setData(self, matrix):
+        self.matrix = matrix
+        self.setSize(len(matrix), len(matrix[0]))
+        self._updateEntries(matrix)
+
+    def getData(self):
+        return self.matrix
+
+    def updateData(self):  
+        print(":)")
+        rows = len(self.entries)
+        columns = len(self.entries[0]) if self.entries else 0
+
+
+        matrix = []
+
+        for i in range(rows):
+            matrix_row = []
+            for j in range(columns):
+                cell_value = self.entries[i][j].get()
+                try:
+                    cell_value = float(cell_value)
+                except ValueError:
+                    cell_value = 0.0  
+                matrix_row.append(cell_value)
+            matrix.append(matrix_row)
+
+        self.matrix = matrix
+
+    def _updateEntries(self, matrix):
+        for i, row in enumerate(matrix):
+            for j, value in enumerate(row):
+                if i < len(self.entries) and j < len(self.entries[i]):
+                    self.entries[i][j].insert(0, value)
+
 class MatrixApp(tk.Tk):
     def __init__(self, data_manager):
         super().__init__()
@@ -42,51 +83,50 @@ class MatrixApp(tk.Tk):
 
         self.center_window( self.winfo_screenwidth() / 4,  self.winfo_screenheight() / 4) 
 
-        # Pola do wprowadzania rozmiaru macierzy
+
+        self._initWindowElements()
+
+        self.m1 = GuiMatrix(self.matrix_a_frame, 3, 3)
+        self.m2 = GuiMatrix(self.matrix_b_frame, 3, 1)
+
+        self.unknowns, matrix_a_data, matrix_b_data = self.data_manager.load_data()
+        
+        self.m1.setData(matrix_a_data)
+        self.m2.setData(matrix_b_data)
+        self.row_entry.insert(0, str(self.unknowns))
+       
+
+    def _initWindowElements(self):
         self.row_label = tk.Label(self, text="Niewiadome:")
         self.row_label.grid(row=0, column=0)
         self.row_entry = tk.Entry(self, width=5, bg=entry_bg, fg=text_color)
         self.row_entry.grid(row=0, column=1)
 
-        # Przycisk do ustawiania rozmiaru macierzy
         self.set_size_btn = tk.Button(self, text="Set Matrix Size", command=self.set_matrix_size, bg=button_bg, fg=text_color)
         self.set_size_btn.grid(row=0, column=2, columnspan=1)
 
-        self.matrix_a_frame = tk.Frame(self)  # Ramka na macierz a
+        self.matrix_a_frame = tk.Frame(self) 
         self.matrix_a_frame.grid(row=3, column=0, columnspan=3)
 
-        self.matrix_b_frame = tk.Frame(self)  # Ramka na macierz b
+        self.matrix_b_frame = tk.Frame(self) 
         self.matrix_b_frame.grid(row=3, column=4, columnspan=3)
 
         self.columnconfigure(3, minsize=30)
 
-        # Przycisk do aktualizacji macierzy
         self.update_btn = tk.Button(self, text="Update Matrix", command=self.update_matrix, bg=button_bg, fg=text_color)
         self.update_btn.grid(row=4, column=0, columnspan=2)
 
         self.matrix_label = tk.Label(self, text="Matrix Values: ", fg='grey')
         self.matrix_label.grid(row=5, column=0, columnspan=2)
 
-        self.m1 = GuiMatrix(self.matrix_a_frame, 3, 3)
-        self.m2 = GuiMatrix(self.matrix_b_frame, 3, 1)
+   
 
-        self.m1.update_size(3, 3)
-        self.m2.update_size(3, 1)
 
-        # Ładowanie danych z pliku przy starcie
-        # rows, columns, matrix_a_data, matrix_b_data = self.data_manager.load_data()
-        # self.row_entry.insert(0, str(rows))
-        # # self.column_entry.insert(0, str(columns))
-        # self.set_matrix_size()  # Ustaw rozmiar macierzy
-        # self.fill_matrix(matrix_a_data, self.entries)  # Wypełnij macierz danymi
-        # self.fill_matrix(matrix_b_data, self.entries2)  # Wypełnij macierz danymi
 
     def center_window(self, width=300, height=200):
-        # Obliczenie współrzędnych środka ekranu
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
 
-        # Obliczenie położenia okna, aby znalazło się na środku ekranu
         x = (screen_width / 2) - (width / 2)
         y = (screen_height / 2) - (height / 2)
 
@@ -95,85 +135,21 @@ class MatrixApp(tk.Tk):
     def set_matrix_size(self):
         rows = int(self.row_entry.get())
         columns = int(self.row_entry.get())
-        self.m1.update_size(rows, rows)
-        self.m2.update_size(rows, 1)
+        self.m1.setSize(rows, rows)
+        self.m2.setSize(rows, 1)
 
-    def update_array(self, rows, columns, frame, entries2):
-        # Usuwamy wszystkie poprzednie Entry z ramki
-        for widget in frame.winfo_children():
-            widget.destroy()
-
-        entries2 = []
-        for i in range(rows):
-            row = []
-            for j in range(columns):
-                entry = tk.Entry(frame, width=5, bg=self.entry_bg, fg=self.text_color, borderwidth=2)
-                entry.grid(row=i, column=j, padx=1, pady=1)
-                row.append(entry)
-            entries2.append(row)
-
-
-    def fill_matrix(self, matrix_data, entries2):
-        for i, row in enumerate(matrix_data):
-            for j, value in enumerate(row):
-                if i < len(entries2) and j < len(entries2[i]):
-                    entries2[i][j].insert(0, value)
-
-
-    # def update_matrix(self):
-    #     rows = len(self.entries)
-    #     columns = len(self.entries[0]) if self.entries else 0
-    #     # Zamiana pustych pól na zera
-    #     matrix = [[self.entries[i][j].get() if self.entries[i][j].get() != '' else '0' for j in range(columns)] for i in range(rows)] if self.entries else []
-    #     self.data_manager.save_data(rows, columns, matrix)
-    #     values = "\n".join([" ".join(str(cell) if cell != '' else '0' for cell in row) for row in matrix])
-    #     self.matrix_label.config(text="Matrix Values:\n" + values)
                     
     def update_matrix(self):
-        rows = len(self.entries)
-        columns = len(self.entries[0]) if self.entries else 0
+        self.m1.updateData()
+        self.m2.updateData()
 
-        rows2 = len(self.entries2)
-        columns2 = len(self.entries2[0]) if self.entries2 else 0
+        print(self.m1.getData())
+        print(self.m2.getData())
 
-        matrix = []
-
-        matrix2 = []
-
-        for i in range(rows):
-            matrix_row = []
-            for j in range(columns):
-                cell_value = self.entries[i][j].get()
-                # Próba konwersji wartości komórki na liczbę zmiennoprzecinkową
-                try:
-                    cell_value = float(cell_value)
-                except ValueError:
-                    cell_value = 0.0  # Ustawiamy 0.0 jeśli konwersja nie powiedzie się
-                matrix_row.append(cell_value)
-            matrix.append(matrix_row)
-
-        for i in range(rows2):
-            matrix_row = []
-            for j in range(columns2):
-                cell_value = self.entries2[i][j].get()
-                # Próba konwersji wartości komórki na liczbę zmiennoprzecinkową
-                try:
-                    cell_value = float(cell_value)
-                except ValueError:
-                    cell_value = 0.0  # Ustawiamy 0.0 jeśli konwersja nie powiedzie się
-                matrix_row.append(cell_value)
-            matrix2.append(matrix_row)
+        self.data_manager.save_data(self.unknowns, self.m1.getData(), self.m2.getData())
 
 
-        print(matrix)
-        print(matrix2)
-
-        # Zapisywanie do pliku JSON (nadpisujemy funkcję save_data, aby obsługiwała liczby całkowite)
-        self.data_manager.save_data(rows, columns, matrix, matrix2)
-
-        # Aktualizowanie etykiety z wartościami macierzy
-#        norm = MatrixCalculations().normalize(matrix)
-        values = "\n".join([" ".join(map(lambda x: f"{x:.2f}", row)) for row in matrix])
+        values = "\n".join([" ".join(map(lambda x: f"{x:.2f}", row)) for row in self.m1.getData()])
         self.matrix_label.config(text="Matrix Values:\n" + values)
 
   
--- matrix_data_manager.py
+++ matrix_data_manager.py
@@ -9,29 +9,27 @@ class MatrixDataManager:
         try:
             with open(self.filepath, 'r') as file:
                 data = json.load(file)
-            return data.get('rows'), data.get('columns'), data.get('matrix_a'), data.get('matrix_b')
-        except FileNotFoundError:
-            return 3, 3, [['' for _ in range(3)] for _ in range(3)], [['' for _ in range(3)] for _ in range(1)]  # Zwraca domyślną macierz 3x3 jeśli plik nie istnieje
+            return data.get('unknowns'), data.get('matrix_a'), data.get('matrix_b')
+        except:
+            return 3, [['' for _ in range(3)] for _ in range(3)], [['' for _ in range(1)] for _ in range(3)]  # Zwraca domyślną macierz 3x3 jeśli plik nie istnieje
 
-    def save_data(self, rows, columns, matrix_a, matrix_b):
+    def save_data(self, unknowns, matrix_a, matrix_b):
         data = {
-            'rows': rows,
-            'columns': columns,
+            'unknowns': unknowns,
             'matrix_a': matrix_a,
             'matrix_b': matrix_b
         }
-        # with open(self.filepath, 'w') as file:
-        #     file.write('{\n')
-        #     file.write('    "rows": ' + str(rows) + ',\n')
-        #     file.write('    "columns": ' + str(columns) + ',\n')
-        #     file.write('    "matrix_a": [\n')
-        #     for row in matrix_a[:-1]:
-        #         file.write('        ' + json.dumps(row) + ',\n')
-        #     file.write('        ' + json.dumps(matrix_a[-1]) + '\n')
-        #     file.write('    ],\n')
-        #     file.write('    "matrix_b": [\n')
-        #     for row in matrix_b[:-1]:
-        #         file.write('        ' + json.dumps(row) + ',\n')
-        #     file.write('        ' + json.dumps(matrix_b[-1]) + '\n')
-        #     file.write('    ]\n')
-        #     file.write('}\n')
+        with open(self.filepath, 'w') as file:
+            file.write('{\n')
+            file.write('    "unknowns": ' + str(unknowns) + ',\n')
+            file.write('    "matrix_a": [\n')
+            for row in matrix_a[:-1]:
+                file.write('        ' + json.dumps(row) + ',\n')
+            file.write('        ' + json.dumps(matrix_a[-1]) + '\n')
+            file.write('    ],\n')
+            file.write('    "matrix_b": [\n')
+            for row in matrix_b[:-1]:
+                file.write('        ' + json.dumps(row) + ',\n')
+            file.write('        ' + json.dumps(matrix_b[-1]) + '\n')
+            file.write('    ]\n')
+            file.write('}\n')
